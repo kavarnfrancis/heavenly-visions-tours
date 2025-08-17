0e151b788b7f4d236cdd17a56a2e5d03308167ed
BluejayChroniclair <churaa1@student.op.ac.nz>
2025-08-18 00:28:12 +1200

    style: made the booking page and main css file look better with consistent colours

--- booking.js
+++ booking.js
@@ -1,3 +1,31 @@
 
+// extra selection choices
+const packageSel = document.getElementById('packagesel');
+const accommodationSel = document.getElementById('accommodationSel');
+const cateringSel = document.getElementById('cateringSel');
+
+// extra selection expanding
+const bedSection = document.getElementById('bedSection');
+const allergenSection = document.getElementById('allergenSection');
+const addressSection = document.getElementById('addressSection');
+
+const nutsSection = document.getElementById('nutsSection');
+
+// extra selection more options
+const doubleBedInput = document.getElementsByName('doubleBedInput');
+const singleBedInput = document.getElementsByName('singleBedInput');
+
+const dairySel = document.getElementsByName('dairy');
+const glutenSel = document.getElementsByName('gluten');
+const soySel = document.getElementsByName('soy');
+const nutsSel = document.getElementsByName('nuts');
+
+const peanutSel = document.getElementsByName('peanut');
+const pinenutSel = document.getElementsByName('pinenut');
+const walnutSel = document.getElementsByName('walnut');
+
+const nutMiscSel = document.getElementsByName('nutMisc');
+
+
 
 
