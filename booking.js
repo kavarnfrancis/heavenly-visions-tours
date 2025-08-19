ba26f0db9872ed679ebb8c715139362fe0717e26
Ari <churaa1@student.op.ac.nz>
2025-08-19 17:35:15 +1200

    feat: added req check and clear button

--- booking.js
+++ booking.js
@@ -1,10 +1,9 @@
 
 // extra selection choices
 const tourSel = document.getElementsByName('tourSel'); // radio
-const packageSel = document.getElementsByName('packagesel');
+const packageSel = document.getElementsByName('packageSel');
 const accommodationSel = document.getElementById('accommodationSel'); // checkbox
 const cateringSel = document.getElementById('cateringSel');
-
 
 
 // extra selection expanding
@@ -18,51 +17,144 @@ const nutsSection = document.getElementById('nutsSection');
 const doubleBedInput = document.getElementsByName('doubleBedInput');
 const singleBedInput = document.getElementsByName('singleBedInput');
 
-const dairySel = document.getElementsByName('dairy');
-const glutenSel = document.getElementsByName('gluten');
-const soySel = document.getElementsByName('soy');
-const nutsSel = document.getElementsByName('nuts');
+const dietToggle = document.getElementsByName('diet');
+const dietMisc = document.getElementsByName('dietMisc');
+
+
+const allergyToggle = document.getElementsByName('allergen');
 const allergyMisc = document.getElementsByName('allergyMisc');
+
+
+const reqField = document.getElementsByClassName('reqField');
+
+const allInputs = document.getElementsByTagName('input');
+const clearFormBtn = document.getElementById('clearButton');
+const checkoutBtn = document.getElementById('checkoutButton');
+
+
+clearFormBtn.addEventListener('click', clearOrder);
+
+
+
+
+function checkReqField(check, value, text){
+
+    let foundField;
+
+    Array.from(reqField).forEach (field => {
+        if (field.getAttribute('value') == value){
+            foundField = field
+        }
+    });
+    
+    if (foundField != null) {
+        foundField.innerHTML = check ? `${text}` : `${text} <span class="redText">*</span>`;
+    } else {
+        console.log("req field not valid.")
+        console.log(foundField);
+    }
+}
+
+let tour = '';
+let tourCost = 0;
+let tourSelected = false;
+
+// tours
+for (let selection of tourSel){
+    selection.addEventListener('change', (event => {
+        tourSelected = true;
+        checkReqField(tourSelected, 'tour', 'Tour')
+
+        switch (selection.value){
+            case 'halberryPass':
+                tour = 'Halberry Pass';
+                tourCost = 20;
+            break;
+            case 'pintoReserve':
+                tour = 'Pinto Reserve';
+                tourCost = 20;
+            break;
+            case 'bulderOutlook':
+                tour = 'Bulder Outlook';
+                tourCost = 20;
+            break;
+        }
+
+        console.log(selection.value);
+        updateOrder();
+        
+    }));
+}
+
+let package = '';
+let packageCost = 0;
+let packageSelected = false;
+
+// package
+for (let selection of packageSel){
+
+    selection.addEventListener('change', (event => {
+        packageSelected = true;
+        checkReqField(packageSelected, 'package', 'Package');
+
+        if (selection.value == 'standard'){
+            packageCost = 0;
+        } else {
+            packageCost = 20;
+        }
+        console.log(selection.value);
+        updateOrder();
+    }));
+}
+
 
 
 
 function updateOrder(){
+    let cost = tourCost + packageCost;
+    
+
 
 }
 
+function clearOrder(){
+    Array.from(allInputs).forEach(input =>
+    {
+        switch (input.type){
+            case 'checkbox' :
+            case 'radio':
+                input.checked = false;
+            break;
+            
+            default:
+                input.value = "";
+            break;
+        }
+    });
 
-let tour = '';
-let tourCost = 0;
-// tours
-for (let selection of tourSel){
-    selection.addEventListener('change', (event => {
-    if (selection.value == 'halberryPass'){
-    tour = 'Halberry Pass';
-    tourCost = 20;
-    } else if (selection.value == 'pintoReserve'){
+    tour = '';
+    tourCost = 0;
+    tourSelected = false;
+    checkReqField(tourSelected, 'tour', 'Tour');
+    package = '';
+    packageCost = 0;
+    packageSelected = false;
+    checkReqField(packageSelected, 'package', 'Package');
 
-    } else {
 
-    }
-    console.log(selection.value);
     updateOrder();
-    }));
 }
 
 
-// package
-for (let selection of packageSel){
-    selection.addEventListener('change', (event => {
-    if (selection.value == 'standard'){
 
-    } else {
+let addressEntered = false;
+let bedsEntered = false;
 
-    }
-    console.log(selection.value);
-    updateOrder();
-    }));
-}
 
+checkReqField(tourSelected, 'tour', 'Tour')
+checkReqField(packageSelected, 'package', 'Package');
+checkReqField(addressEntered, 'addressInput', 'Address');
+checkReqField(bedsEntered, 'bedsInput', 'Beds');
 
 
 
